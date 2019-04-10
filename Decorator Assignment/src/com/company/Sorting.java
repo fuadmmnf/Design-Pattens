1d62760cbfe8d8db7b63420704bf0ceeedead19e
fuadmmnf <bsse0920@iit.du.ac.bd>
2019-04-10 23:23:07 +0600

    Decorator assignment problem

--- Decorator Assignment/src/com/company/Sorting.java
+++ Decorator Assignment/src/com/company/Sorting.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Sorting {
+
+    private List<Student> students;
+
+    public Sorting() {
+
+    }
+
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public List<Student> sort(int pref) {
+        Collections.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+
+                Object type = s1.getAttributeList().get(pref).getClass();
+
+                if (s1.getAttributeList().get(pref) == s2.getAttributeList().get(pref))
+                    return 0;
+                return s1.getAttributeList().get(pref) < s2.getAttributeList().get(pref)? -1 : 1;
+            }
+        });
+        return students;
+    }
 }
--- Decorator Assignment/src/com/company/Student.java
+++ Decorator Assignment/src/com/company/Student.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private int age;
@@ -7,5 +10,38 @@ public class Student {
     private String session;
 
 
+    private List<Object> attributeList;
 
+    public Student(String name, int age, double cgpa, String session) {
+        this.name = name;
+        this.age = age;
+        this.cgpa = cgpa;
+        this.session = session;
+
+        attributeList = new ArrayList<>();
+        attributeList.add(this.name);
+        attributeList.add(this.age);
+        attributeList.add(this.cgpa);
+        attributeList.add(this.session);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public List<Object> getAttributeList() {
+        return attributeList;
+    }
 }
